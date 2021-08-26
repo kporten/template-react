ddcd5dafe253c30174ddee85be95a9bad64d3f67
Kevin Porten <hello@kevinporten.dev>
2021-08-26 20:27:27 +0200

    feat: add eslint rules

--- src/index.tsx
+++ src/index.tsx
@@ -17,5 +17,3 @@ ReactDOM.render(
 // ? https://web.dev/vitals/
 // ? If you want to report web vitals, you can pass a report handler (https://github.com/GoogleChrome/web-vitals#reporthandler)
 reportWebVitals().catch(console.error);
-
-export default App;
