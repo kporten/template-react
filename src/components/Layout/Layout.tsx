82c7250c593a58349347d313ffbac66f8122013e
Kevin Porten <hello@kevinporten.dev>
2021-10-03 21:32:59 +0200

    feat: add theme color

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Layout: React.FC = ({ children }) => (
-  <main className="flex items-center justify-center h-screen bg-gradient-to-r from-cyan-400 to-sky-500 text-white">
+  <main className="flex items-center justify-center h-screen bg-gradient-to-b from-cyan-400 dark:from-cyan-600 to-sky-500 dark:to-sky-700 text-white">
     {children}
   </main>
 );
