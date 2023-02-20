6b588d05fbeb6d6d80808d557c1e26bf666c9716
Kevin Porten <hello@kevinporten.dev>
2023-02-20 10:17:45 +0100

    fix(next): add data img src to csp

--- apps/next/next.config.js
+++ apps/next/next.config.js
@@ -33,6 +33,7 @@ const contentSecurityPolicy = `
   default-src 'self';
   script-src 'self' 'unsafe-inline';
   style-src 'self' 'unsafe-inline';
+  img-src 'self' data:;
   frame-ancestors 'none';
   object-src 'none';
 `;
