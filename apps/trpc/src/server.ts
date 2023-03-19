0603773ea85b4a756e17dbb0488e3d3594a710c1
Kevin Porten <hello@kevinporten.dev>
2023-03-19 18:08:55 +0100

    add graceful shutdown

--- apps/trpc/src/server.ts
+++ apps/trpc/src/server.ts
@@ -15,8 +15,34 @@ app.use('/trpc', trpc);
 
 app.get('/', (req, res) => res.sendStatus(200));
 
-app.listen(env.PORT, () => {
+const server = app.listen(env.PORT, () => {
   console.info(`➜ Local: http://localhost:${env.PORT}`);
+});
+
+const shutdown = async () => {
+  console.info('➜ Shutdown initiated...');
+
+  await new Promise<void>((resolve, reject) => {
+    server.close((error) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve();
+      }
+    });
+  });
+
+  console.info('➜ Shutdown finished!');
+};
+
+process.on('SIGINT', async () => {
+  await shutdown();
+  process.exit(0);
+});
+
+process.on('SIGTERM', async () => {
+  await shutdown();
+  process.exit(0);
 });
 
 export { type AppRouter } from '@/middlewares/trpc';
