80dbbcade5065bcc55958c03082920a3682ce245
Kevin Porten <hello@kevinporten.dev>
2023-07-02 20:44:51 +0200

    feat: update button

--- apps/vite/src/components/button.test.tsx
+++ apps/vite/src/components/button.test.tsx
@@ -122,6 +122,20 @@ describe('size', () => {
       </DocumentFragment>
     `);
   });
+
+  it('should render size icon', () => {
+    const { asFragment } = render(<Button size="icon">icon</Button>);
+
+    expect(asFragment()).toMatchInlineSnapshot(`
+      <DocumentFragment>
+        <button
+          class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 w-10"
+        >
+          icon
+        </button>
+      </DocumentFragment>
+    `);
+  });
 });
 
 describe('props', () => {
--- apps/vite/vite.config.ts
+++ apps/vite/vite.config.ts
@@ -5,30 +5,34 @@ import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [reactPlugin(), visualizer()],
-  resolve: {
-    alias: {
-      '@': path.resolve('./src'),
+export default defineConfig((env) => {
+  return {
+    plugins: [reactPlugin(), visualizer()],
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+      },
     },
-  },
-  define: {
-    // https://github.com/jotaijs/jotai-devtools/issues/82
-    'process.platform': `'${process.platform}'`,
-  },
-  // https://vitest.dev/config/
-  test: {
-    coverage: {
-      provider: 'v8',
-      branches: 90,
-      lines: 90,
-      exclude: ['**/__mocks__/**', '**/test/**'],
-      reporter: ['text', 'html'],
+    ...(env.mode === 'development' && {
+      define: {
+        // https://github.com/jotaijs/jotai-devtools/issues/82
+        'process.platform': `'${process.platform}'`,
+      },
+    }),
+    // https://vitest.dev/config/
+    test: {
+      coverage: {
+        provider: 'v8',
+        branches: 90,
+        lines: 90,
+        exclude: ['**/__mocks__/**', '**/test/**'],
+        reporter: ['text', 'html'],
+      },
+      environment: 'happy-dom',
+      setupFiles: ['./src/test/setup.ts'],
+      typecheck: {
+        include: ['**/*.{ts,tsx}'],
+      },
     },
-    environment: 'happy-dom',
-    setupFiles: ['./src/test/setup.ts'],
-    typecheck: {
-      include: ['**/*.{ts,tsx}'],
-    },
-  },
+  };
 });
